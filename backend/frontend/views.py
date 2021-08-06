0b7ba97650490b847eeae3baa515d045a0ee5a3b
Kristian Krøgenes <kristiankrogenes@gmail.com>
2021-08-06 16:20:22 +0200

    Styled pages and added footer

--- backend/frontend/views.py
+++ backend/frontend/views.py
@@ -42,3 +42,10 @@ class CourseView(APIView):
         courses = Course.objects.all()
         serializer = CourseSerializer(courses, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = CourseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
